48f0de4babcc221b1dc9cf2542444c7cbfde8682
Pavel Grinchenko <psdcoder@gmail.com>
2020-03-16 12:54:17 +0300

    Add 404 page

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express()
 
 const apiMiddleware = require('./middleware/api')
 const redirectsMiddleware = require('./middleware/redirects')
+const notFoundMiddleware = require('./middleware/notFound')
 
 const port = process.env.PORT || 3000
 
@@ -12,5 +13,7 @@ app.use(redirectsMiddleware)
 app.use('/api', apiMiddleware)
 
 app.use(express.static('public'))
+
+app.use(notFoundMiddleware)
 
 app.listen(port, () => console.log(`Ready on localhost:${port}!`))
